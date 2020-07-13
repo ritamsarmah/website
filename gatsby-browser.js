582886f3daf8f23e1406455e6a0868b099b00d5a
Ritam Sarmah <sarmah.ritam@gmail.com>
2020-07-13 16:49:43 -0700

    Disable scroll position saving

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,14 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
+exports.shouldUpdateScroll = ({
+  routerProps: { location },
+  getSavedScrollPosition,
+}) => {
+  const currentPosition = getSavedScrollPosition(location)
+  const queriedPosition = getSavedScrollPosition({ pathname: `/random` })
+
+  window.scrollTo(...(currentPosition || [0, 0]))
+
+  return false
+}
